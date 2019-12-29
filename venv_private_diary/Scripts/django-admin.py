388db09f38509e17fc30a4dc33bbc98de1a49986
yuta29 <47266960+yuta29@users.noreply.github.com>
2019-12-29 17:37:03 +0900

    構成変更と環境構築

---
+++ venv_private_diary/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yuta8\documents\develop\django_private_diary\venv_private_diary\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
